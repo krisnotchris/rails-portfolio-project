42e8bc803ac6b6e3ec7a9e0ed6f63fbc6d59e8e6
krisnotchris <krishenderson@gmail.com>
2016-11-10 23:40:44 -0600

    created relationships between models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :recipes
+  resources :ingredients
   root to: 'users#index'
   devise_for :users
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111045732) do
+ActiveRecord::Schema.define(version: 20161111052011) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
