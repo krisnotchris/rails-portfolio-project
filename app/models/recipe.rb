c306f3332c3bb3a58dc6b919712f2eb4185b6c7d
krisnotchris <krishenderson@gmail.com>
2016-11-11 20:28:17 -0600

    added validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,9 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :comments
   belongs_to :user
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  
 end
