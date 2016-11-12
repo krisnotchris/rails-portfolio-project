394667d17e64b36f57c431c9fabe1f16461fe3f6
krisnotchris <krishenderson@gmail.com>
2016-11-12 15:41:29 -0600

    created ingredient creation form

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,9 +1,25 @@
 class IngredientsController < ApplicationController
   def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+    redirect_to ingredients_path
   end
 
   def show
-    @ingredients = Ingredient.all
+    @ingredient = Ingredient.find(params[:id])
   end
-  
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:id, :name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :ingredients
-  root 'users#index'
   devise_for :users, controllers: { registrations: 'users/registrations', :omniauth_callbacks => 'users/omniauth_callbacks' }
+  root 'users#index'
+  resources :ingredients
+  resources :recipes do
+    resources :comments
+    resources :recipe_ingredients, only: [:index, :create, :update]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
