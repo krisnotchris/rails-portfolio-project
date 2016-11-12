c75dff2597a52484e31ce56da38ef883a400cdfc
krisnotchris <krishenderson@gmail.com>
2016-11-12 16:13:54 -0600

    create recipe complete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,26 @@
 class RecipesController < ApplicationController
   def index
-    
     @recipes = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    @recipe.ingredients = params[:recipe][:ingredient_ids]
+
+    if @recipe.save
+      redirect_to recipe_recipe_ingredients_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :name, :rating, :description, :ingredient_ids => [], :ingredient_attributes => [:name])
   end
 end
