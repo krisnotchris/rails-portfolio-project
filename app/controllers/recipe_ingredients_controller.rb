7d7ef4a4f80d87e9881da76c33a3afcd22da6954
krisnotchris <krishenderson@gmail.com>
2016-11-13 13:55:53 -0600

    working on quantity updater

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,9 +1,14 @@
 class RecipeIngredientsController < ApplicationController
+
   def index
-    
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe_ingredients = @recipe.ingredients
-    @recipess = @recipe.recipe_ingredients
+    @recipe_ingredients = @recipe.recipe_ingredients
+  end
+
+  def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_ingredients = RecipeIngredient.find
+    @ingredient_id = params["ingredient_id"]
   end
 
 end
