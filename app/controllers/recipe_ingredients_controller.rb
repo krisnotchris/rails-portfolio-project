0dda2ff892d0d04e04300ac284b3635b390d1fea
krisnotchris <krishenderson@gmail.com>
2016-11-13 22:58:05 -0600

    default values on form working

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -6,16 +6,12 @@ class RecipeIngredientsController < ApplicationController
     @recipe_ingredients = RecipeIngredient.where(recipe_id: @recipe_id)
   end
 
-  def edit
-    @recipe = Recipe.find(params[:recipe_id])
-    @ingredient_id = params["ingredient_id"]
-  end
-
   def create
     redirect_to recipe_recipe_ingredients_path
   end
 
   def update
+
     redirect_to recipe_recipe_ingredients_path
   end
 
