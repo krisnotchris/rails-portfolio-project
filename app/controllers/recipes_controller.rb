1e758a335686bafb057ebc1358ec13c2679b63be
krisnotchris <krishenderson@gmail.com>
2016-11-17 00:43:20 -0600

    working on update ingredients function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+  end
+
   def create
     @recipe = Recipe.create(recipe_params)
 
