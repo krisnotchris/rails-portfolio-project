6e37f7549e71d393814e60768c1163c340353151
krisnotchris <krishenderson@gmail.com>
2016-11-23 09:49:57 -0600

    final push before assessment

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,22 +1,19 @@
 class RecipeIngredientsController < ApplicationController
-  before_action :auth_user
+  before_action :auth_user, :recipe_finder
+
   def index
-    recipe_finder
     @recipe_id = params[:recipe_id].to_i
     @recipe_ingredients = RecipeIngredient.where(recipe_id: @recipe_id)
   end
 
   def update
-    recipe_finder
     @recipe_ingredients = @recipe.recipe_ingredients
     @ing = @recipe_ingredients.find_by(ingredient_id: params[:id])
     @ing.update(recipe_ingredients_params)
-
     redirect_to recipe_path(@recipe.id)
   end
 
   def edit
-    recipe_finder
     @recipe_ingredient = @recipe.recipe_ingredients
     @ing = @recipe_ingredient.find_by(ingredient_id: params[:id])
   end
